e9af7bf8be3ce2469e7e57ae907e14c9650a73c9
sed-i <82407168+sed-i@users.noreply.github.com>
2022-06-29 13:31:31 -0400

    Fix relation specificity

--- tests/integration/test_rerelate.py
+++ tests/integration/test_rerelate.py
@@ -74,9 +74,9 @@ async def test_build_and_deploy(ops_test: OpsTest, prometheus_charm, prometheus_
 
     await asyncio.gather(
         ops_test.model.add_relation(app_name, tester_app_name),
-        ops_test.model.add_relation(app_name, "alertmanager"),
-        ops_test.model.add_relation(app_name, "grafana:grafana-source"),
-        ops_test.model.add_relation(app_name, "grafana-agent:send-remote-write"),
+        ops_test.model.add_relation(f"{app_name}:alertmanager", "alertmanager"),
+        ops_test.model.add_relation(f"{app_name}:grafana-source", "grafana"),
+        ops_test.model.add_relation(f"{app_name}:receive-remote-write", "grafana-agent"),
     )
     await ops_test.model.wait_for_idle(status="active", timeout=600)
 
@@ -98,8 +98,8 @@ async def test_remove_relation(ops_test: OpsTest):
 async def test_rerelate(ops_test: OpsTest):
     await asyncio.gather(
         ops_test.model.add_relation(app_name, tester_app_name),
-        ops_test.model.add_relation(app_name, "alertmanager"),
-        ops_test.model.add_relation(app_name, "grafana:grafana-source"),
-        ops_test.model.add_relation(app_name, "grafana-agent:send-remote-write"),
+        ops_test.model.add_relation(f"{app_name}:alertmanager", "alertmanager"),
+        ops_test.model.add_relation(f"{app_name}:grafana-source", "grafana"),
+        ops_test.model.add_relation(f"{app_name}:receive-remote-write", "grafana-agent"),
     )
     await ops_test.model.wait_for_idle(status="active", timeout=600)
