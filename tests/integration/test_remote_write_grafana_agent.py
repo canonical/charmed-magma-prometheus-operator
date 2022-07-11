4dd983c3d77850f5ba8ab50785c9bef415bf60e7
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2022-07-11 11:33:34 +0200

    Uncommenting tests

--- tests/integration/test_remote_write_grafana_agent.py
+++ tests/integration/test_remote_write_grafana_agent.py
@@ -110,42 +110,42 @@ async def test_remote_write_alerts_deduplicate(ops_test):
     assert len(tester_rules) == 1
 
 
-# @pytest.mark.abort_on_fail
-# async def test_check_data_persist_on_kubectl_delete_pod(ops_test, prometheus_charm):
-#     prometheus_app_name = "prometheus"
-#     pod_name = f"{prometheus_app_name}-0"
-#     query = "prometheus_tsdb_head_chunks_created_total{}"
-#     total0 = await run_promql(ops_test, query, prometheus_app_name)
-#
-#     # total0 is a list of dicts in which "value" is a list that contains
-#     # the timestamp and the value itself.
-#     num_head_chunks_before = int(total0[0]["value"][1])
-#     assert num_head_chunks_before > 0
-#
-#     cmd = [
-#         "sg",
-#         "microk8s",
-#         "-c",
-#         " ".join(["microk8s.kubectl", "delete", "pod", "-n", ops_test.model_name, pod_name]),
-#     ]
-#
-#     logger.debug(
-#         "Removing pod '%s' from model '%s' with cmd: %s", pod_name, ops_test.model_name, cmd
-#     )
-#
-#     retcode, stdout, stderr = await ops_test.run(*cmd)
-#     assert retcode == 0, f"kubectl failed: {(stderr or stdout).strip()}"
-#     logger.debug(stdout)
-#
-#     await ops_test.model.block_until(
-#         lambda: len(ops_test.model.applications[prometheus_app_name].units) > 0
-#     )
-#     await ops_test.model.wait_for_idle(apps=[prometheus_app_name], status="active", timeout=60)
-#     assert await check_prometheus_is_ready(ops_test, prometheus_app_name, 0)
-#
-#     total1 = await run_promql(ops_test, query, prometheus_app_name)
-#     num_head_chunks_after = int(total1[0]["value"][1])
-#     assert num_head_chunks_before <= num_head_chunks_after
+@pytest.mark.abort_on_fail
+async def test_check_data_persist_on_kubectl_delete_pod(ops_test, prometheus_charm):
+    prometheus_app_name = "prometheus"
+    pod_name = f"{prometheus_app_name}-0"
+    query = "prometheus_tsdb_head_chunks_created_total{}"
+    total0 = await run_promql(ops_test, query, prometheus_app_name)
+
+    # total0 is a list of dicts in which "value" is a list that contains
+    # the timestamp and the value itself.
+    num_head_chunks_before = int(total0[0]["value"][1])
+    assert num_head_chunks_before > 0
+
+    cmd = [
+        "sg",
+        "microk8s",
+        "-c",
+        " ".join(["microk8s.kubectl", "delete", "pod", "-n", ops_test.model_name, pod_name]),
+    ]
+
+    logger.debug(
+        "Removing pod '%s' from model '%s' with cmd: %s", pod_name, ops_test.model_name, cmd
+    )
+
+    retcode, stdout, stderr = await ops_test.run(*cmd)
+    assert retcode == 0, f"kubectl failed: {(stderr or stdout).strip()}"
+    logger.debug(stdout)
+
+    await ops_test.model.block_until(
+        lambda: len(ops_test.model.applications[prometheus_app_name].units) > 0
+    )
+    await ops_test.model.wait_for_idle(apps=[prometheus_app_name], status="active", timeout=60)
+    assert await check_prometheus_is_ready(ops_test, prometheus_app_name, 0)
+
+    total1 = await run_promql(ops_test, query, prometheus_app_name)
+    num_head_chunks_after = int(total1[0]["value"][1])
+    assert num_head_chunks_before <= num_head_chunks_after
 
 
 @pytest.mark.abort_on_fail
